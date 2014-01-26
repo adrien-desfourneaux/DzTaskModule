e7d5552add329001e614d198fa00288ac81a086c
Dieze <dieze51@gmail.com>
2014-01-26 19:26:35 +0100

    Add parameter to run spec or cept or all tests

--- script/run_tests.sh
+++ script/run_tests.sh
@@ -8,5 +8,11 @@
 # SCRIPTPATH = zf2_app/module/DzTask/script
 SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
 cd $SCRIPTPATH/..
-../../vendor/bin/phpspec run
-../../vendor/bin/codecept run
+
+if [ $# -eq 0 ]; then
+    ../../vendor/bin/phpspec run
+    ../../vendor/bin/codecept run
+fi;
+
+if [ "$1" = "spec" ]; then ../../vendor/bin/phpspec run; fi
+if [ "$1" = "cept" ]; then ../../vendor/bin/codecept run; fi
