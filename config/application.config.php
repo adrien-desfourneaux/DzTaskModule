b63854241a337ad765d8d1c4e5cf8eeaa0e18698
Dieze <dieze51@gmail.com>
2014-01-28 01:03:48 +0100

    Move test/development application.config.php to config/

--- config/application.config.php
+++ config/application.config.php
@@ -2,13 +2,19 @@
 /**
  * Test application configuration file.
  *
+ * This file contains only the modules really needed for
+ * the DzTask module to work.
+ *
+ * This file should be run either by /public/dz-task.php
+ * or /public/test/dz-task.php
+ *
  * PHP version 5.3.3
  *
  * @category Config
  * @package  DzTask
  * @author   Adrien Desfourneaux (aka Dieze) <dieze51@gmail.com>
  * @license  http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2
- * @link     https://github.com/dieze/DzTask/blob/master/tests/application.config.php
+ * @link     https://github.com/dieze/DzTask/blob/master/config/application.config.php
  */
 return array(
     'modules' => array(
