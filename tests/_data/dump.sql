d50713856e4bd0d060bae824e23017650d0b3d45
Dieze <dieze51@gmail.com>
2014-02-05 14:03:42 +0100

    Copy create sql instructions to sql dump file for tests

--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -1,22 +1,21 @@
-/* Replace this file with actual dump of your database */
-
 CREATE TABLE task
 (
-    task_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    task_id     INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     description VARCHAR(200) NOT NULL UNIQUE,
-    begin_date INTEGER NOT NULL,
-    end_date INTEGER NOT NULL,
-    state_id INTEGER NOT NULL,
+    begin_date  INTEGER NOT NULL,
+    end_date    INTEGER NOT NULL,
+    state_id    INTEGER NOT NULL,
     FOREIGN KEY (state_id) REFERENCES task_state (state_id)
 );
 
 CREATE TABLE task_state
 (
 	state_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	label    VARCHAR(20) NOT NULL UNIQUE
+	label    VARCHAR(20) NOT NULL UNIQUE,
+	icon_url VARCHAR(200)
 );
 
-INSERT INTO task_state (state_id, label) VALUES (1, 'Pas commencé');
-INSERT INTO task_state (state_id, label) VALUES (2, 'En cours');
-INSERT INTO task_state (state_id, label) VALUES (3, 'Fait');
-INSERT INTO task_state (state_id, label) VALUES (4, 'En retard');
+INSERT INTO task_state (state_id, label, icon_url) VALUES (1, 'Pas commencé', '/img/DzTask/not-started.png');
+INSERT INTO task_state (state_id, label, icon_url) VALUES (2, 'En cours', '/img/DzTask/in-progress.png');
+INSERT INTO task_state (state_id, label, icon_url) VALUES (3, 'Fait', '/img/DzTask/done.png');
+INSERT INTO task_state (state_id, label, icon_url) VALUES (4, 'En retard', '/img/DzTask/late.png');
