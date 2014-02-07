8334c576210c7507f2a13d9f4694c2f61d499bba
Dieze <dieze51@gmail.com>
2014-02-07 14:59:17 +0100

    Rename ShowAllTasksToVisitor to ShowAllTasks, add task_state class in <tr>

---
+++ tests/acceptance/ShowAllTasksCept.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Test d'acceptation ShowAllTasks
+ * Afficher toutes les taches.
+ *
+ * PHP version 5.3.3
+ *
+ * @category   Test
+ * @package    DzTask
+ * @subpackage Acceptance
+ * @author     Adrien Desfourneaux (aka Dieze) <dieze51@gmail.com>
+ * @license    http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2
+ * @link       https://github.com/dieze/DzTask/blob/master/tests/acceptance/ShowAllTasksCept.php
+ */
+
+$I = new WebGuy($scenario);
+$I->wantTo('Voir toutes les taches');
+
+// Insère les données par défaut
+// du fichier data/dztask.dump.sqlite.sql
+// dans la base de données
+$I->haveDefaultTaskStatesInDatabase();
+$I->haveDefaultTasksInDatabase();
+
+$I->amOnPage('/task/show-all');
+
+$I->see('Tâches');
+
+$I->see('Description');
+$I->see('Période');
+$I->see('Etat');
+
+$I->seeElement(".not-started");
+$I->seeElement(".in-progress");
+$I->seeElement(".done");
+$I->seeElement(".late");
+
+$I->see("Tâche non débutée");
+$I->see("Tâche qui débute aujourd'hui");
+$I->see("Tâche en cours 1");
+$I->see("Tâche en cours 2");
+$I->see("Tâche qui se termine aujourd'hui");
+$I->see("Tâche faite");
+$I->see("Tâche en retard");
