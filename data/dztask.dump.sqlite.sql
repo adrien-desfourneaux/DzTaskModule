57af9cb0fd159285dc9a9dc447783626626c73a7
Dieze <dieze51@gmail.com>
2014-02-07 13:30:35 +0100

    Move task_state inserts to dump, use strftime() in dump

--- data/dztask.dump.sqlite.sql
+++ data/dztask.dump.sqlite.sql
@@ -1,12 +1,139 @@
-/*
-Timestamps
-01/01/2013	1356994800 
-01/01/2014	1388530800 
-01/01/2015	1420066800 
-01/01/2016  1451602800 
-*/
+INSERT INTO task_state (
+    state_id,
+    label,
+    icon_url
+) VALUES (
+    1,
+    'not-started',
+    '/img/DzTask/not-started.png'
+);
 
-INSERT INTO task (task_id, description, begin_date, end_date, state_id) VALUES (1, 'Tache pas commencée', 1420066800, 1451602800, 1);
-INSERT INTO task (task_id, description, begin_date, end_date, state_id) VALUES (2, 'Tache faite', 1356994800, 1388530800, 3);
-INSERT INTO task (task_id, description, begin_date, end_date, state_id) VALUES (3, 'Tache en cours', 1356994800, 1420066800, 2);
-INSERT INTO task (task_id, description, begin_date, end_date, state_id) VALUES (4, 'Tache en retard', 1356994800, 1388530800, 4);
+INSERT INTO task_state (
+    state_id,
+    label,
+    icon_url
+) VALUES (
+    2,
+    'in-progress',
+    '/img/DzTask/in-progress.png'
+);
+
+INSERT INTO task_state (
+    state_id,
+    label,
+    icon_url
+) VALUES (
+    3,
+    'done',
+    '/img/DzTask/done.png'
+);
+
+INSERT INTO task_state (
+    state_id,
+    label,
+    icon_url
+) VALUES (
+    4,
+    'late',
+    '/img/DzTask/late.png'
+);
+
+/* --------------------  */
+
+INSERT INTO task (
+    task_id,
+    description,
+    begin_date,
+    end_date,
+    state_id
+) VALUES (
+    1,
+    'Tâche non débutée',
+    strftime('%s', 'now', 'start of day', '-1 hour', '+6 months'),
+    strftime('%s', 'now', 'start of day', '-1 hour', '+1 year'),
+    1
+);
+
+INSERT INTO task (
+    task_id,
+    description,
+    begin_date,
+    end_date,
+    state_id
+) VALUES (
+    2,
+    "Tâche qui débute aujourd'hui",
+    strftime('%s', 'now', 'start of day', '-1 hour'),
+    strftime('%s', 'now', 'start of day', '-1 hour', '+2 years'),
+    1
+);
+
+INSERT INTO task (
+    task_id,
+    description,
+    begin_date,
+    end_date,
+    state_id
+) VALUES (
+    3,
+    "Tâche en cours 1",
+    strftime('%s', 'now', 'start of day', '-1 hour', '-1 day'),
+    strftime('%s', 'now', 'start of day', '-1 hour', '+3 years'),
+    2
+);
+
+INSERT INTO task (
+    task_id,
+    description,
+    begin_date,
+    end_date,
+    state_id
+) VALUES (
+    4,
+    "Tâche en cours 2",
+    strftime('%s', 'now', 'start of day', '-1 hour', '-1 day'),
+    strftime('%s', 'now', 'start of day', '-1 hour', '+3 years'),
+    2
+);
+
+INSERT INTO task (
+    task_id,
+    description,
+    begin_date,
+    end_date,
+    state_id
+) VALUES (
+    5,
+    "Tâche qui se termine aujourd'hui",
+    strftime('%s', 'now', 'start of day', '-1 hour', '-1 day'),
+    strftime('%s', 'now', 'start of day', '-1 hour'),
+    2
+);
+
+INSERT INTO task (
+    task_id,
+    description,
+    begin_date,
+    end_date,
+    state_id
+) VALUES (
+    6,
+    'Tâche faite',
+    strftime('%s', 'now', 'start of day', '-1 hour', '-2 days'),
+    strftime('%s', 'now', 'start of day', '-1 hour', '-1 day'),
+    3
+);
+
+INSERT INTO task (
+    task_id,
+    description,
+    begin_date,
+    end_date,
+    state_id
+) VALUES (
+    7,
+    'Tâche en retard',
+    strftime('%s', 'now', 'start of day', '-1 hour', '-2 days'),
+    strftime('%s', 'now', 'start of day', '-1 hour', '-1 day'),
+    4
+);
--- data/dztask.sqlite.sql
+++ data/dztask.sqlite.sql
@@ -1,3 +1,10 @@
+CREATE TABLE task_state
+(
+    state_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    label    VARCHAR(20) NOT NULL UNIQUE,
+    icon_url VARCHAR(200)
+);
+
 CREATE TABLE task
 (
     task_id     INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -7,15 +14,3 @@ CREATE TABLE task
     state_id    INTEGER NOT NULL,
     FOREIGN KEY (state_id) REFERENCES task_state (state_id)
 );
-
-CREATE TABLE task_state
-(
-	state_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	label    VARCHAR(20) NOT NULL UNIQUE,
-	icon_url VARCHAR(200)
-);
-
-INSERT INTO task_state (state_id, label, icon_url) VALUES (1, 'Pas commencé', '/img/DzTask/not-started.png');
-INSERT INTO task_state (state_id, label, icon_url) VALUES (2, 'En cours', '/img/DzTask/in-progress.png');
-INSERT INTO task_state (state_id, label, icon_url) VALUES (3, 'Fait', '/img/DzTask/done.png');
-INSERT INTO task_state (state_id, label, icon_url) VALUES (4, 'En retard', '/img/DzTask/late.png');
